e67c825ecaa50b51b34f9a0489aba31aec5744a6
Cihangir SAVAS <cihangir@koding.com>
2015-01-22 00:45:30 -0800

    Test: make tests go1.1 compatible

--- validator_test.go
+++ validator_test.go
@@ -9,11 +9,11 @@ import (
 )
 
 // assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+func assert(t *testing.T, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
+		t.FailNow()
 	}
 }
 
